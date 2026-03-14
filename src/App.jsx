d92b14ea4cd91ada4c2727cd6ba269271c65ca10
SonaliRavichandran <sonaliravichandran99@gmail.com>
2026-03-14 22:33:26 +0000

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+  
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Call Dashboard</h1>
+  
+    </div>
+
+
+  )
+}
+
+export default App
